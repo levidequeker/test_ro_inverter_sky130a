148569fa1e21f3d7298dc78967c7930e9e8aa7fb
levidequeker <levi.dequeker@gmail.com>
2026-02-18 18:13:42 +0100

    Added nfet geometries csv's

--- scripts/parse_spice_model_cards.py
+++ scripts/parse_spice_model_cards.py
@@ -1,27 +1,31 @@
+import os
 import re
 import pandas as pd
 
 # Path to your .pm3.spice file
-spice_file = os.path.join("/opt/pdk/share/pdk/sky130A/libs.ref/sky130_fd_pr/spice","sky130_fd_pr__nfet_01v8_lvt.pm3.spice")
+spice_file = "sky130_fd_pr__nfet_03v3_nvt.pm3.spice"
+spice_path = os.path.join("/opt/pdk/share/pdk/sky130A/libs.ref/sky130_fd_pr/spice",spice_file)
 
 # Regular expression to match a model line with Lmin, Lmax, Wmin, Wmax
 model_regex = re.compile(
-    r'^\.model\s+(\S+)\s+.*Lmin=([\d\.Ee+-]+).*Lmax=([\d\.Ee+-]+).*Wmin=([\d\.Ee+-]+).*Wmax=([\d\.Ee+-]+)',
+    r'__model\.[0-9]*\s.*\n',
     re.IGNORECASE
 )
-
+dimensions_regex = re.compile(r'\+\sLmin=([\d\.Ee+-]+).*Lmax=([\d\.Ee+-]+).*Wmin=([\d\.Ee+-]+).*Wmax=([\d\.Ee+-]+) ', re.IGNORECASE)
 models = []
-
-with open(spice_file, 'r') as f:
+dimensions_line_ctr = 0
+with open(spice_path, 'r') as f:
     for line in f:
-        line = line.strip()
         match = model_regex.search(line)
         if match:
-            model_name = match.group(1)
-            lmin = float(match.group(2))
-            lmax = float(match.group(3))
-            wmin = float(match.group(4))
-            wmax = float(match.group(5))
+            print(line)
+            model_name = re.findall(r'model\.[0-9]*', line)
+            dimensions_line_ctr = 3
+        if dimensions_line_ctr == 1:
+            lmin = re.findall(r'lmin\s=\s\d*\.\d*e-\d*', line)[0][7:]
+            lmax = re.findall(r'lmax\s=\s\d*\.\d*e-\d*', line)[0][7:]
+            wmin = re.findall(r'wmin\s=\s\d*\.\d*e-\d*', line)[0][7:]
+            wmax = re.findall(r'wmax\s=\s\d*\.\d*e-\d*', line)[0][7:]
             models.append({
                 "Model": model_name,
                 "Lmin (µm)": lmin,
@@ -29,12 +33,16 @@ with open(spice_file, 'r') as f:
                 "Wmin (µm)": wmin,
                 "Wmax (µm)": wmax
             })
+        if dimensions_line_ctr > 0:
+            dimensions_line_ctr -= 1
 
 # Convert to DataFrame for easy display
 df = pd.DataFrame(models)
 
 # Optional: save to CSV
-df.to_csv("nfet_01v8_lvt_models.csv", index=False)
+df.to_csv(spice_file[:-10] + "_models"  + ".csv", index=False)
+
+pd.set_option('display.max_rows', None)
 
 # Print table
 print(df)
